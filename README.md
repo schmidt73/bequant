2e3f56608cce95c22a9a3d846d03f7e087f95f6b
Henri Schmidt <packerfan73@gmail.com>
2021-06-02 14:09:17 -0400

    Add ALL_PAM library processing functionality.

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# be-analysis
+# Processing Sensor Outcomes
+
+To analyze sensor outcomes from raw FASTq reads, we first performed
+standard paired-end alignment using the PANDAseq tool. This generated
+a pair of merged FASTq files, one for each replicate. This pair of
+merged FASTq files was then used directly for sensor outcome analysis.
+
+We first ensured that the merged FASTq reads were not defective as an
+artifact of sequencing or cloning. Namely, we first threw out reads
+with broken linker constructs and non-matching 5' or 3'
+scaffolds. Then, we used the 5' scaffold and linker construct to
+associate an sgRNA with the read. sgRNAs that did not match those in
+our whitelist were thrown out, as they correspond to
+cloning/oligo-construction? errors. With this set of reads, we then
+aligned the sensor construct in the read to the target sensor
+construct found in the whitelist and classified the outcome. This
+yielded a set of outcomes for each sgRNA that we screened.
+
+Sensor constructs of length different than that of the target sensor
+were classified as indels; we did not distinguish these further. If
+the lengths matched, we classified the outcome as a set of pairs
+$(position, mutation_type)$ where $mutation\_type$ could be one of the
+six single nucleotide mutations. Typically, most of this information
+was thrown out in downstream analysis as we were only concerned with
+editing at substrate nucleotides.
+
 
 
--- src/be_analysis/core.clj
+++ src/be_analysis/core.clj
@@ -9,15 +9,25 @@
 
 ;;;; Data loading code 
 
-(defn get-sensor
+(defn get-pam
   [s]
   (let [lscaffold "AAAAAGTGGCACCGAGTCGGTGCTTTTTTT"
-        rscaffold "GAATTC"]
+        rscaffold "GCATGAATTC"]
+    (if-let [lindex (string/last-index-of s lscaffold)]
+      (if-let [rindex (string/last-index-of s rscaffold)]
+        (if (< lindex rindex) 
+          (subs s (- rindex 10) (- rindex 7)))))))
+
+(defn get-sensor
+  [format s]
+  (let [lscaffold "AAAAAGTGGCACCGAGTCGGTGCTTTTTTT"
+        rscaffold (get {:with-pam "GCATGAATTC" :no-pam "GAATTC"} format)]
     (if-let [lindex (string/last-index-of s lscaffold)]
       (if-let [rindex (string/last-index-of s rscaffold)]
         (if (< lindex rindex) 
           (subs s (+ lindex (count lscaffold)) rindex))))))
 
+; good
 (defn get-sg-rna
   [s]
   (let [lscaffold "CACC"
@@ -26,15 +36,6 @@
       (if-let [rindex (string/index-of s rscaffold)]
         (if (< lindex rindex)
           (subs s (+ lindex (count lscaffold)) rindex))))))
-
-(defn lazily-load-sensors
-  [rdr]
-  (->> (line-seq rdr)
-       (map-indexed (fn [idx item] (if (= 0 (mod (dec idx) 4)) item)))
-       (filter identity)
-       (map get-sensor)
-       (filter identity)
-       (filter #(= 40 (count %)))))
 
 (defn load-guide-csv-mbes-row
   [row]
@@ -56,21 +57,25 @@
      :pam (nth row 31)
      :edit-pos edit-pos}))
 
+(defn load-guide-csv-all-pams-row
+  [row]
+  (let [edit-pos (Integer/parseInt (nth row 2))
+        target (subs (nth row 4) 117 157)]
+    {:sg-rna (nth row 3)
+     :edit-pos edit-pos
+     :pam (nth row 1)
+     :target target
+     :guide-id (nth row 0)}))
+
 (defn lazily-load-guides
   [rdr row-reader]
   (->> (rest (csv/read-csv rdr))
        (map row-reader)))
 
-(defn load-mbes-guides-file
-  [guides-csv-file]
+(defn load-guides-file
+  [row-reader guides-csv-file]
   (with-open [rdr (io/reader guides-csv-file)]
-    (->> (lazily-load-guides rdr load-guide-csv-mbes-row)
-         (vec))))
-
-(defn load-hbes-guides-file
-  [guides-csv-file]
-  (with-open [rdr (io/reader guides-csv-file)]
-    (->> (lazily-load-guides rdr load-guide-csv-hbes-row)
+    (->> (lazily-load-guides rdr row-reader)
          (vec))))
 
 ;;;; Data analysis code
@@ -156,7 +161,9 @@
 (defn pretty-print-outcomes-edit-pos-row
   [guide outcome-rep1 outcome-rep2 edit-pos writer]
   (let [guide-id (:guide-id guide)
+        sequence (:sg-rna guide)
         pam (:pam guide)
+        ex-pam (subs (:target guide) (+ 31 (count pam)) (+ 33 (count pam)))
         nuc-context (subs (:target guide) (max (- edit-pos 2) 0) (+ edit-pos 3))
         total-rep1  (+ (get outcome-rep1 :edits 0) (get outcome-rep1 :indels 0))
         percent-rep1 #(float (if (= total-rep1 0) 0 (* 100 (/ % total-rep1))))
@@ -170,7 +177,7 @@
         c-to-t-nc-rep2 (get outcome-rep2 {:from \C :to \T :position edit-pos :distance 1} 0)
         c-to-t-rep2 (total-edit-count outcome-rep2 \C \T edit-pos)
         c-to-g-rep2 (total-edit-count outcome-rep2 \C \G edit-pos)
-        line (string/join "," [guide-id edit-pos pam nuc-context
+        line (string/join "," [guide-id sequence pam ex-pam edit-pos nuc-context
                                total-rep1 c-to-t-rep1 c-to-a-rep1 c-to-g-rep1 c-to-t-nc-rep1
                                total-rep2 c-to-t-rep2 c-to-a-rep2 c-to-g-rep2 c-to-t-nc-rep2
                                (percent-rep1 c-to-t-rep1) (percent-rep1 c-to-a-rep1)
@@ -206,7 +213,8 @@
 
 (defn pretty-print-outcomes-edit-pos-csv
   [outcomes-rep1 outcomes-rep2 writer]
-  (let [header (str "guide_ID,cytosine_position,PAM,surrounding nucleotide context (NNCNN),"
+  (let [header (str "guide_ID,sequence,PAM,exPAM,"
+                    "cytosine_position,surrounding nucleotide context (NNCNN),"
                     "total_REP1,tCTN_REP1,tCAN_REP1,tCGN_REP1,tCT_REP1,"
                     "total_REP2,tCTN_REP2,tCAN_REP2,tCGN_REP2,tCT_REP2,"
                     "percent_tCTN_REP1,percent_tCAN_REP1,percent_tCGN_REP1,percent_tCT_REP1,"
@@ -234,8 +242,34 @@
 
 ;;;; Code that actually does data analysis
 
-(defn analyze-fastq-file-with-progress
-  [guides fastq-file]
+(defmulti analyze-fastq-file-with-progress
+  (fn [screen-type _ _]
+    (get {:all-pam :with-pam :hbes :no-pam :mbes :no-pam}
+         screen-type)))
+
+(defmethod analyze-fastq-file-with-progress
+  :with-pam
+  [_ guides fastq-file]
+  (let [counter (atom 0)
+        sg-rna-and-pam-to-guide (into {} (map #(vector (select-keys % [:sg-rna :pam]) %) guides))]
+    (with-open [reader (io/reader fastq-file)]
+      (->> (line-seq reader)
+           (map-indexed (fn [idx item] (if (= 0 (mod (dec idx) 4)) item)))
+           (filter identity)
+           (map #(vector (get sg-rna-and-pam-to-guide {:sg-rna (get-sg-rna %) :pam (get-pam %)})
+                         (get-sensor :with-pam %)
+                         {:sg-rna (get-sg-rna %) :pam (get-pam %)}))
+           (filter #(and (some? (first %)) (some? (second %))))
+           (map #(do
+                   (when (= 0 (mod @counter 10000))
+                     (timbre/info (str "FASTq Records Processed: " @counter)))
+                   (swap! counter inc)
+                   %))
+           (count-outcomes)))))
+
+(defmethod analyze-fastq-file-with-progress
+  :no-pam
+  [_ guides fastq-file]
   (let [counter (atom 0)
         sg-rna-to-guide (into {} (map #(vector (:sg-rna %) %) guides))]
     (with-open [reader (io/reader fastq-file)]
@@ -243,7 +277,7 @@
            (map-indexed (fn [idx item] (if (= 0 (mod (dec idx) 4)) item)))
            (filter identity)
            (map #(vector (get sg-rna-to-guide (get-sg-rna %))
-                         (get-sensor %)))
+                         (get-sensor :no-pam %)))
            (filter #(and (some? (first %)) (some? (second %))))
            (map #(do
                    (when (= 0 (mod @counter 10000))
@@ -253,16 +287,24 @@
            (count-outcomes)))))
 
 (def guides-map
-  {:mbes (load-mbes-guides-file (io/resource "MBESv4_revised_whitelist.csv"))
-   :hbes (load-hbes-guides-file (io/resource "HBESv4_revised_whitelist.csv"))})
+  {:mbes    (load-guides-file load-guide-csv-mbes-row
+                              (io/resource "MBESv4_revised_whitelist.csv"))
+   :hbes    (load-guides-file load-guide-csv-hbes-row
+                              (io/resource "HBESv4_revised_whitelist.csv"))
+   :all-pam (load-guides-file load-guide-csv-all-pams-row
+                              (io/resource "ALL_PAM_whitelist.csv"))})
+
+;; (analyze-fastq-file-with-progress :all-pam
+;;                                   (:all-pam guides-map)
+;;                                   "/home/schmidt73/tmp/fastqs/ALL_PAMS_RA2_REP1/merged.fastq")
 
 (defn process-fastqs-edit-position
   [output-file rep1 rep2 screen-type format]
   (let [guides (screen-type guides-map)
         outcomes-rep1 (do (timbre/info "Processing FASTq file:" rep1)
-                          (analyze-fastq-file-with-progress guides rep1))
+                          (analyze-fastq-file-with-progress screen-type guides rep1))
         outcomes-rep2 (do (timbre/info "Processing FASTq file:" rep2)
-                          (analyze-fastq-file-with-progress guides rep2))]
+                          (analyze-fastq-file-with-progress screen-type guides rep2))]
     (with-open [writer (io/writer output-file)]
       (case format
         :target (pretty-print-outcomes-csv outcomes-rep1 outcomes-rep2 writer)
@@ -276,7 +318,7 @@
     :default :mbes
     :default-desc "MBES"
     :parse-fn #(keyword (string/lower-case %))
-    :validate [#{:mbes :hbes} "must be one of ['MBES', 'HBES']"]]
+    :validate [#{:mbes :hbes :all-pam} "must be one of ['MBES', 'HBES', 'ALL-PAM']"]]
    ["-f" "--format OUTPUT_FORMAT" "Output either only target cytosine or all cytosines."
     :default :target
     :default-desc "TARGET"
